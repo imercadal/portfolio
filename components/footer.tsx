ec243554fbbcc62dc1391eb1b7a7afa9b23f63dd
imercadal <ima280@nyu.edu>
2025-09-29 16:10:55 -0400

    react-icon typing fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,6 +5,9 @@ import { MdEmail } from 'react-icons/md';
 
 
 const MyIcon: IconType = MdEmail;
+type Item = {
+  icon: IconType;
+};
 
 const navigation = [
     {
@@ -41,29 +44,31 @@ const navigation = [
   ]
 
 
-export default function Footer() {
-    return(
-        <footer>
-            <div className="md:h-16 h-24 mx-auto max-w-7xl px-6 py-6 md:flex md:items-center md:justify-around lg:px-20">
-                <div className="flex justify-center items-center gap-x-6 md:order-2">
-                    {navigation.map((item) => (
-                        <a 
-                          key={item.name} 
-                          href={item.href} 
-                          className="dark hover:text-accent"
-                          target='_blank'
-                          rel='noopener noreferrer'
-                        >
-                        <span className="sr-only">{item.name}</span>
-                        <item.icon aria-hidden="true" className="size-6" />
-                        </a>
-                    ))}
-                </div>
-                <p className="mt-2 text-center text-xs/6 text-dark md:order-1 md:mt-0">
-                    &copy; 2025 Developed by Irene Mercadal Albornoz.
-                </p>
-            </div>
-        </footer>
+export default function Footer({ item }: { item: Item }) {
+  const Icon = item.icon;
 
-    )
+  return(
+    <footer>
+        <div className="md:h-16 h-24 mx-auto max-w-7xl px-6 py-6 md:flex md:items-center md:justify-around lg:px-20">
+            <div className="flex justify-center items-center gap-x-6 md:order-2">
+                {navigation.map((item) => (
+                    <a 
+                      key={item.name} 
+                      href={item.href} 
+                      className="dark hover:text-accent"
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                    <span className="sr-only">{item.name}</span>
+                    <Icon aria-hidden="true" className="w-6 h-6" />
+                    </a>
+                ))}
+            </div>
+            <p className="mt-2 text-center text-xs/6 text-dark md:order-1 md:mt-0">
+                &copy; 2025 Developed by Irene Mercadal Albornoz.
+            </p>
+        </div>
+    </footer>
+
+  )
 }
