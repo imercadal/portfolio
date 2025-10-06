e80b971afe2e2ac19df5ffa3dd325e7556d026b5
imercadal <ima280@nyu.edu>
2025-10-06 12:03:31 -0400

    Switched homepage video to vimeo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,74 +1,80 @@
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function About() {
     return (
-        <div className="container mx-auto  max-w-4xl px-4 py-24 pt-36">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mx-auto">
-                <div className="relative aspect-[3/4] w-full sm:w-1/2 md:w-full mx-auto sm:px-0">
-                    <Image src="/10_Irenes-headshot.webp" alt="Irene Mercadal" fill className="object-cover" />
-                </div>
-                <div className="space-y-6 text-sm">
-                    <p className="text-base font-bold">Hi! I&apos;m Irene, a filmmaker  and interdisciplinary artist.</p>
-                    <p>
-                        My <Link href='/directing' className="font-bold">films</Link> explore a variety of topics, 
-                        including environmentalism, power dynamics and social justice, and I&apos;m particularly interested 
-                        in exploring the female gaze.
-                    </p>
-                    <p>
-                        My debut feature film <Link href='/directing/aguas-y-restos' className="font-bold">Aguas y restos</Link> is in development 
-                        and is currently part of the <a href="https://www.torinofilmlab.it/" rel="noopener noreferrer">Torino Film Lab&apos;s</a> Green Narratives 
-                        development workshop. My short film Baby Steps was just finished and will premiere soon. And although I specialize in film, I also 
-                        work with mixed media, I am a full stack developer, and collaborate with digital artists, theater makers and performers, particularly 
-                        within the company I&apos;m a part of, <a href="https://glitchworks.org/" rel="noopener noreferrer" className="font-bold"> Glitch</a>.
-                    </p>
-                    <p>
-                        I grew up in Talca, Chile, and  I&apos;ve been living in NYC for the past seven years. In that period, I&apos;ve graduated with a Film MFA 
-                        from NYU Tisch and I&apos;ve been Director of Photography on numerous short narrative films. My work is shaped by my bachelor degree 
-                        in film aesthetics, and my previous work in documentary film back in Chile.
-                    </p>
-                    <p>
-                        Besides dedicating some time to my projects as writer/director, I&apos;m interested in collaborating with other people, 
-                        whether as a DP or in any capacity that will allow me to explore, grow and learn. So if my work looks like down your alley, hit me up!
-                    </p>
-                </div>
-            </div>
-            <div className="flex flex-col md:flex-row text-sm/7 pt-8">
-                <div className="flex sm:w-1/2 pb-6 md:pb-0 bg-red-10">
-                    <h2 className="w text-base font-light mb-2">CONTACT</h2>
-                    <div className="flex-2 pl-6 sm:pl-8 md:pl-12 bg-green-10">
-                        <a
-                            href="mailto:irenemercadal@gmail.com"
-                            rel="noopener noreferrer"
-                        >
-                            <p className="pl-3">Email — irenemercadal@gmail.com</p>
-                        </a>
-                        <a
-                            href="https://www.instagram.com/imercadal/"
-                            rel="noopener noreferrer"
-                        >
-                            <p className="pl-3">Instagram — imercadal</p>
-                        </a>
-                        <a
-                            href="https://www.linkedin.com/in/irene-mercadal/"
-                            rel="noopener noreferrer"
-                        >
-                            <p className="pl-3">LinkedIn — irene-mercadal</p>
-                        </a>
+        <>
+            <Head>
+                <title>Irene Mercadal | About</title>
+                <meta name="keywords" content="filmmaker"/>
+            </Head>
+            <div className="container mx-auto  max-w-4xl px-4 py-24 pt-36">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mx-auto">
+                    <div className="relative aspect-[3/4] w-full sm:w-1/2 md:w-full mx-auto sm:px-0">
+                        <Image src="/10_Irenes-headshot.webp" alt="Irene Mercadal" fill className="object-cover" />
+                    </div>
+                    <div className="space-y-6 text-sm">
+                        <p className="text-base font-bold">Hi! I&apos;m Irene, a filmmaker  and interdisciplinary artist.</p>
+                        <p>
+                            My <Link href='/directing' className="font-bold">films</Link> explore a variety of topics, 
+                            including environmentalism, power dynamics and social justice, and I&apos;m particularly interested 
+                            in exploring the female gaze.
+                        </p>
+                        <p>
+                            My debut feature film <Link href='/directing/aguas-y-restos' className="font-bold">Aguas y restos</Link> is in development 
+                            and is currently part of the <a href="https://www.torinofilmlab.it/" rel="noopener noreferrer">Torino Film Lab&apos;s</a> Green Narratives 
+                            development workshop. My short film Baby Steps was just finished and will premiere soon. And although I specialize in film, I also 
+                            work with mixed media, I am a full stack developer, and collaborate with digital artists, theater makers and performers, particularly 
+                            within the company I&apos;m a part of, <a href="https://glitchworks.org/" rel="noopener noreferrer" className="font-bold"> Glitch</a>.
+                        </p>
+                        <p>
+                            I grew up in Talca, Chile, and  I&apos;ve been living in NYC for the past seven years. In that period, I&apos;ve graduated with a Film MFA 
+                            from NYU Tisch and I&apos;ve been Director of Photography on numerous short narrative films. My work is shaped by my bachelor degree 
+                            in film aesthetics, and my previous work in documentary film back in Chile.
+                        </p>
+                        <p>
+                            Besides dedicating some time to my projects as writer/director, I&apos;m interested in collaborating with other people, 
+                            whether as a DP or in any capacity that will allow me to explore, grow and learn. So if my work looks like down your alley, hit me up!
+                        </p>
                     </div>
                 </div>
-                <div className="flex sm:w-1/2">
-                    <h2 className="flex-1 text-base font-light mb-2">AWARDS</h2>
-                    <div className="flex-2 pl-12">
-                        <p>Volker Bahnemann Award for Outstanding Cinematography, awarded by ARRI 2022</p>
-                        <p>Nestor Almendros Award for Best Cinematography, First Run Film Festival 2025</p>
+                <div className="flex flex-col md:flex-row text-sm/7 pt-8">
+                    <div className="flex sm:w-1/2 pb-6 md:pb-0 bg-red-10">
+                        <h2 className="w text-base font-light mb-2">CONTACT</h2>
+                        <div className="flex-2 pl-6 sm:pl-8 md:pl-12 bg-green-10">
+                            <a
+                                href="mailto:irenemercadal@gmail.com"
+                                rel="noopener noreferrer"
+                            >
+                                <p className="pl-3">Email — irenemercadal@gmail.com</p>
+                            </a>
+                            <a
+                                href="https://www.instagram.com/imercadal/"
+                                rel="noopener noreferrer"
+                            >
+                                <p className="pl-3">Instagram — imercadal</p>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/irene-mercadal/"
+                                rel="noopener noreferrer"
+                            >
+                                <p className="pl-3">LinkedIn — irene-mercadal</p>
+                            </a>
+                        </div>
                     </div>
+                    <div className="flex sm:w-1/2">
+                        <h2 className="flex-1 text-base font-light mb-2">AWARDS</h2>
+                        <div className="flex-2 pl-12">
+                            <p>Volker Bahnemann Award for Outstanding Cinematography, awarded by ARRI 2022</p>
+                            <p>Nestor Almendros Award for Best Cinematography, First Run Film Festival 2025</p>
+                        </div>
+                    </div>
+
                 </div>
-
             </div>
-        </div>
-
-  )
+        </>
+    )
 }
 
 /**
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,41 @@
+'use client'
+
+import Head from "next/head"
 import Link from "next/link"
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div className="relative">
-      <div className="relative flex items-center justify-center h-screen overflow-hidden z-20">
-        <video
-          src="irene_dpreel_homepage_1080p.mp4"
-          autoPlay loop muted
-          className="absolute z-10 w-auto min-w-full min-h-full max-w-none">
-        </video>
-      </div>
-      <div className="absolute inset-0 flex items-center justify-center z-40">
-        <Link
-          href="/cinematography"
-          className="border-2 border-white px-8 py-3 text-base text-white hover:text-gray-300 hover:border-gray-300 transition-colors duration-100"
+    <>
+      <Head>
+        <title>Irene Mercadal Filmmaker</title>
+        <meta name="keywords" content="cinematographer"></meta>
+      </Head>
+      <div className="relative">
+        <div className="relative flex items-center justify-center h-screen overflow-hidden z-20">
+            <iframe
+              src="https://player.vimeo.com/video/1087396555?h=92d92e82e7&autoplay=1&muted=1&loop=1&background=1"
+              allow="autoplay; fullscreen; picture-in-picture"
+              referrerPolicy="strict-origin-when-cross-origin"
+              title="Homepage Cinematography Reel"
+              className="absolute top-1/2 left-1/2 w-[177.78vh] h-[100vh] -translate-x-1/2 -translate-y-1/2"
+            ></iframe>
+        </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3, ease: "easeOut", delay: 0.6 }}
+          className="absolute inset-0 flex items-center justify-center z-40"
         >
-          VIEW WORK
-        </Link>
+          <Link
+            href="/cinematography"
+            className="border-2 border-white px-8 py-3 text-base text-white hover:text-gray-300 hover:border-gray-300 transition-colors duration-100"
+          >
+            VIEW WORK
+          </Link>
+        </motion.div>
       </div>
-    </div>
+    </>
 
   )
-}
-
-/*
-    <div className="relative min-h-screen w-full overflow-hidden">
-      <div className="fixed inset-0 z-[-1] overflow-hidden">
-        <iframe
-          src="https://player.vimeo.com/video/1087396555?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;muted=1&amp;loop=1&amp;background=1"
-          allow="autoplay; fullscreen"
-          allowFullScreen
-          className="w-full h-full object-cover"
-          aria-hidden="true"
-        ></iframe>
-      </div>
-      <div className="absolute inset-0 bg-black/30"></div>
-      <div className="absolute inset-0 flex items-center justify-center">
-        <Link
-          href="/cinematography"
-          className="border-2 border-white px-8 py-3 text-base text-white hover:bg-white hover:text-black transition-colors duration-300"
-        >
-          VIEW WORK
-        </Link>
-      </div>
-    </div>
-*/
+};
